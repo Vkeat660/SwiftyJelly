305c54165151fb1dc07ef00c30c1f50c735a0a40
Vivian Keating <vkeat660@gmail.com>
2015-03-16 23:01:46 -0400

    Refactored some of the path drawing

--- SwiftyJelly/JellyView.swift
+++ SwiftyJelly/JellyView.swift
@@ -20,10 +20,8 @@ class JellyView: UIView {
     
     var displayLink : CADisplayLink!
     
-    var time : CFTimeInterval = 0
-    
-    
-    
+    var time : CFTimeInterval = 5
+
     override init() {
         super.init()
         displayLink  = CADisplayLink(target: self, selector: "reDraw:")
@@ -32,14 +30,6 @@ class JellyView: UIView {
         let tapGesture = UITapGestureRecognizer(target: self, action: "buttonTapped")
         self.addGestureRecognizer(tapGesture)
         
-        let startPath = UIBezierPath()
-        path.moveToPoint(topLeft)
-        path.addLineToPoint(topRight)
-        path.addLineToPoint(bottomRight)
-        path.addLineToPoint(bottomLeft)
-        path.closePath()
-        
-        shapeLayer.path = startPath.CGPath
         shapeLayer.fillColor = UIColor.orangeColor().CGColor
         
         layer.addSublayer(shapeLayer)
@@ -66,6 +56,8 @@ class JellyView: UIView {
             topRight    = CGPoint(x: x0 + Δx, y: y0 +  0)
             bottomLeft  = CGPoint(x: x0 +  0, y: y0 + Δy)
             bottomRight = CGPoint(x: x0 + Δx, y: y0 + Δy)
+        
+            drawPath()
 
     }
     
@@ -79,34 +71,35 @@ class JellyView: UIView {
         time += link.duration
         
         if time < 4.0 {
-            
-            let offset = CGFloat(1600.0 * time * Double(sin(time * π * 2.0 / 0.4)) * Double(exp(-5.0 * time)))
-            let offset2 = CGFloat(1400.0 * time * Double(sin(time * π * 2.0 / 0.4 + π * 0.9)) * Double(exp(-5.0 * time)))
-            
-            let controlPoint1 = CGPoint(x: (topLeft.x      + topRight.x            )/2,
-                y: (topLeft.y      + topRight.y    - offset2)/2)
-            let controlPoint2 = CGPoint(x: (topRight.x     + bottomRight.x + offset)/2,
-                y: (topRight.y     + bottomRight.y         )/2)
-            let controlPoint3 = CGPoint(x: (bottomRight.x  + bottomLeft.x          )/2,
-                y: (bottomRight.y  + bottomLeft.y  + offset2)/2)
-            let controlPoint4 = CGPoint(x: (bottomLeft.x   + topLeft.x     - offset)/2,
-                y: (bottomLeft.y   + topLeft.y             )/2)
-            
-            let path = UIBezierPath()
-            path.moveToPoint(topLeft)
-            path.addQuadCurveToPoint(topRight,    controlPoint: controlPoint1)
-            path.addQuadCurveToPoint(bottomRight, controlPoint: controlPoint2)
-            path.addQuadCurveToPoint(bottomLeft,  controlPoint: controlPoint3)
-            path.addQuadCurveToPoint(topLeft,     controlPoint: controlPoint4)
-            path.closePath()
-            
-            shapeLayer.path = path.CGPath
-
+           drawPath()
         }
+        
     }
     
     func drawPath(){
         
+        let offset = CGFloat(1600.0 * time * Double(sin(time * π * 2.0 / 0.4)) * Double(exp(-5.0 * time)))
+        let offset2 = CGFloat(1400.0 * time * Double(sin(time * π * 2.0 / 0.4 + π * 0.9)) * Double(exp(-5.0 * time)))
+        
+        let controlPoint1 = CGPoint(x: (topLeft.x      + topRight.x            )/2,
+                                    y: (topLeft.y      + topRight.y    - offset2)/2)
+        let controlPoint2 = CGPoint(x: (topRight.x     + bottomRight.x + offset)/2,
+                                    y: (topRight.y     + bottomRight.y         )/2)
+        let controlPoint3 = CGPoint(x: (bottomRight.x  + bottomLeft.x          )/2,
+                                    y: (bottomRight.y  + bottomLeft.y  + offset2)/2)
+        let controlPoint4 = CGPoint(x: (bottomLeft.x   + topLeft.x     - offset)/2,
+                                    y: (bottomLeft.y   + topLeft.y             )/2)
+        
+        let path = UIBezierPath()
+        path.moveToPoint(topLeft)
+        path.addQuadCurveToPoint(topRight,    controlPoint: controlPoint1)
+        path.addQuadCurveToPoint(bottomRight, controlPoint: controlPoint2)
+        path.addQuadCurveToPoint(bottomLeft,  controlPoint: controlPoint3)
+        path.addQuadCurveToPoint(topLeft,     controlPoint: controlPoint4)
+        path.closePath()
+        
+        shapeLayer.path = path.CGPath
+
     }
     
     deinit {
